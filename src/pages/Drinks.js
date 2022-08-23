15d6259a68eb711971ca6aa05da1d3d0d10695f2
pamelamonteiro <pamelamonteiro_@hotmail.com>
2022-08-23 12:37:02 -0300

    Refatoração do Recipe Cards

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -4,29 +4,23 @@ import { Redirect } from 'react-router-dom';
 import propTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import RecipeCards from '../components/RecipeCards';
+import { fetchRecipeDrinks } from '../services/fetchApi';
 
 class Drinks extends React.Component {
+  componentDidMount() {
+    const { makeSearchDrinks } = this.props;
+    const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    makeSearchDrinks(endpoint);
+  }
+
   render() {
     const { drinks } = this.props;
     if (drinks.length === 1) return <Redirect to={ `/drinks/${drinks[0].idDrink}` } />;
     return (
       <div>
         <Header title="Drinks" />
-        { drinks.map((drink, index) => {
-          const { strDrink, strDrinkThumb } = drink;
-          return (
-            <div data-testid={ `${index}-recipe-card` } key={ index }>
-              <h3 data-testid={ `${index}-card-name` }>{ strDrink }</h3>
-              <img
-                src={ strDrinkThumb }
-                alt={ strDrink }
-                data-testid={ `${index}-card-img` }
-                width="200"
-                height="200"
-              />
-            </div>
-          );
-        }) }
+        <RecipeCards />
         <Footer />
       </div>
     );
@@ -35,13 +29,15 @@ class Drinks extends React.Component {
 
 Drinks.propTypes = {
   drinks: propTypes.arrayOf(propTypes.object).isRequired,
+  makeSearchDrinks: propTypes.func.isRequired,
 };
 
 const mapStateToProps = (store) => ({
   drinks: store.recipeReducer.drinks,
 });
-// const mapDispatchToProps = (dispatch) => ({
-//   userLoginDispatch: (email, password) => dispatch(userLogin(email, password)),
-// });
 
-export default connect(mapStateToProps, null)(Drinks);
+const mapDispatchToProps = (dispatch) => ({
+  makeSearchDrinks: (endpoint) => dispatch(fetchRecipeDrinks(endpoint)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Drinks);
