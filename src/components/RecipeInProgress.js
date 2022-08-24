a39da0ad9c2237097d6c773345efe24fef4dd212
cassiojuniorr <cassiospcjunior@gmail.com>
2022-08-23 23:28:38 -0300

    preparando a pagina

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -1,24 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 function RecipeInProgress({ pageActual, meals, drinks }) {
   const [pageState, setPage] = useState({ page: '' });
   const [recipeState, setRecipe] = useState({ recipes: [] });
+  const [redirectState, setRedirect] = useState({ redirect: false });
+  const [markedState, setMarked] = useState({ marked: false });
 
   useEffect(() => {
     const page = pageActual.includes('Foods') ? 'foods' : 'drinks';
     const pag = drinks.length > 0;
     const recipes = pag ? drinks : meals;
+    const obj = localStorage.getItem('inProgressRecipes');
+    if (recipes === obj) setMarked({ marked: true });
 
     setRecipe({ recipes });
     setPage({ page });
   }, []);
 
-  const scratchIngredient = ({ target }) => { };
+  const scratchIngredient = (id) => {
+    const { recipes } = recipeState;
+    const { page } = pageState;
+    const obj = recipes.find((elm) => (
+      (page === 'foods') ? elm.idMeal === id : elm.idDrinks === elm
+    ));
+    localStorage.setItem('inProgressRecipes', obj);
+  };
+
+  const toggleFinish = () => {
+    setRedirect({ redirect: true });
+  };
 
   const { recipes } = recipeState;
   const { page } = pageState;
+  const { redirect } = redirectState;
+  const { marked } = markedState;
 
   return (
     <div>
@@ -42,17 +60,28 @@ function RecipeInProgress({ pageActual, meals, drinks }) {
             <button type="button" data-testid="share-btn">SHARE</button>
             <button type="button" data-testid="favorite-btn">FAVORITE</button>
             <span data-testid="recipe-category">{elm.strCategory}</span>
-            <span
+            <div
               data-testid={ `${i}-ingredient-step` }
             >
-              {elm.strIngredient.concat(i)}
-              <input type="checkbox" onChange={ scratchIngredient } />
-            </span>
+              <span>{elm.strIngredient.concat(i)}</span>
+              <input
+                type="checkbox"
+                value={ marked }
+                onChange={ scratchIngredient }
+              />
+            </div>
             <span data-testid="instructions">{elm.strInstructions}</span>
-            <button type="button" data-testid="finish-recipe-btn">FINISH</button>
+            <button
+              type="button"
+              data-testid="finish-recipe-btn"
+              onClick={ toggleFinish }
+            >
+              FINISH
+            </button>
           </div>
         ))
       }
+      {redirect && <Redirect to="/done-recipes" />}
     </div>
 
   );
