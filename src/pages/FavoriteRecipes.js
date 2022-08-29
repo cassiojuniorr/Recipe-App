a47b4b62129566d67a50276c70d234c266c6132a
cassiojuniorr <cassiospcjunior@gmail.com>
2022-08-29 20:48:54 -0300

    req 56

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
@@ -10,145 +10,139 @@ import style from '../styles/favoriteRecipes.module.scss';
 
 const copy = require('clipboard-copy');
 
-class FavoriteRecipes extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      filter: 'allBtn',
-      favoriteRecipes: [],
-      copyState: false,
-      favoriteState: true,
-    };
-  }
+function FavoriteRecipes() {
+  const [favoriteRecipes, setFavorite] = useState([]);
+  const [filter, setFilter] = useState('allBtn');
+  const [copyState, setCopy] = useState(false);
 
-  componentDidMount() {
-    this.setState({
-      favoriteRecipes: JSON.parse(localStorage.getItem('favoriteRecipes') || '[]'),
-    });
-  }
+  useEffect(() => {
+    const favorite = JSON.parse(localStorage.getItem('favoriteRecipes')) !== null
+      ? JSON.parse(localStorage.getItem('favoriteRecipes')) : [];
 
-  filterFunc = ({ target: { name } }) => {
+    setFavorite([...favorite]);
+  }, []);
+
+  const filterFunc = ({ target: { name } }) => {
     if (name === 'allBtn') {
-      this.setState({ filter: 'allBtn' });
-    } else {
-      const typ = name === 'foodBtn' ? 'drink' : 'food';
-      this.setState({ filter: typ });
+      setFilter('allBtn');
+      return;
     }
+    const typ = name === 'foodBtn' ? 'drink' : 'food';
+    setFilter(typ);
   };
 
-  toggleShare = (type, id) => {
+  const toggleShare = (type, id) => {
     copy(`${window.location.origin}/${type}s/${id}`);
-    this.setState({ copyState: true });
-    setTimeout(() => this.setState({ copyState: false }), Number('2000'));
+    setCopy(true);
+    setTimeout(() => setCopy(false), Number('2000'));
   };
 
-  makeFave = (type, id, event) => {
-    const { favoriteRecipes, favoriteState } = this.state;
-    const { name } = event.target;
-    toggleFavorite(favoriteRecipes[name], id, type, favoriteState);
-    this.setState({ favoriteRecipes: !favoriteRecipes });
+  const makeFave = (id) => {
+    const favoriteStore = JSON.parse(localStorage.getItem('favoriteRecipes')) !== null
+      ? JSON.parse(localStorage.getItem('favoriteRecipes')) : [];
+
+    const filterFav = favoriteStore.filter((rcp) => rcp.id !== id);
+
+    localStorage.setItem('favoriteRecipes', JSON.stringify(filterFav));
+
+    setFavorite([...filterFav]);
   };
 
-  searchByBarOn = () => {}
+  const searchByBarOn = () => {};
+  searchByBarOn();
 
-  render() {
-    this.searchByBarOn();
-    const { favoriteRecipes, filter, copyState, favoriteState } = this.state;
-    const { containerFavorite } = style;
-    return (
-      <div>
-        <Header title="Favorite Recipes" searchByBarOn={ this.searchByBarOn } />
-        <div className={ containerFavorite }>
-          <button
-            type="button"
-            data-testid="filter-by-all-btn"
-            name="allBtn"
-            onClick={ this.filterFunc }
-          >
-            All
-          </button>
+  const { containerFavorite } = style;
+  return (
+    <div>
+      <Header title="Favorite Recipes" searchByBarOn={ searchByBarOn } />
+      <div className={ containerFavorite }>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          name="allBtn"
+          onClick={ filterFunc }
+        >
+          All
+        </button>
 
-          <button
-            type="button"
-            data-testid="filter-by-food-btn"
-            name="foodBtn"
-            onClick={ this.filterFunc }
-          >
-            Food
-          </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          name="foodBtn"
+          onClick={ filterFunc }
+        >
+          Food
+        </button>
 
-          <button
-            type="button"
-            name="drinksBtn"
-            data-testid="filter-by-drink-btn"
-            onClick={ this.filterFunc }
-          >
-            Drinks
-          </button>
-          {
-            favoriteRecipes !== undefined && favoriteRecipes !== null
-            && (
-              favoriteRecipes.filter((rcp) => rcp.type !== filter)
-                .map((recipe, index) => (
-                  <div key={ recipe.id }>
-                    <Link to={ `/${recipe.type}s/${recipe.id}` }>
-                      <img
-                        src={ recipe.image }
-                        alt={ recipe.name }
-                        data-testid={ `${index}-horizontal-image` }
-                        width="300px"
-                      />
-                    </Link>
+        <button
+          type="button"
+          name="drinksBtn"
+          data-testid="filter-by-drink-btn"
+          onClick={ filterFunc }
+        >
+          Drinks
+        </button>
 
-                    <span data-testid={ `${index}-horizontal-top-text` }>
-                      {
-                        recipe.type === 'food'
-                          ? `${recipe.nationality} - ${recipe.category}`
-                          : `${recipe.alcoholicOrNot}`
-                      }
-                    </span>
+        {
+          favoriteRecipes.filter((recipe) => recipe.type !== filter)
+            .map((recipe, index) => (
+              <div key={ recipe.id }>
+                <Link to={ `/${recipe.type}s/${recipe.id}` }>
+                  <img
+                    src={ recipe.image }
+                    alt={ recipe.name }
+                    data-testid={ `${index}-horizontal-image` }
+                    width="300px"
+                  />
+                </Link>
 
-                    <Link
-                      to={ `/${recipe.type}s/${recipe.id}` }
-                    >
-                      <span
-                        data-testid={ `${index}-horizontal-name` }
-                      >
-                        {recipe.name}
-                      </span>
-                    </Link>
+                <span data-testid={ `${index}-horizontal-top-text` }>
+                  {
+                    recipe.type === 'food'
+                      ? `${recipe.nationality} - ${recipe.category}`
+                      : `${recipe.alcoholicOrNot}`
+                  }
+                </span>
 
-                    {copyState && <h1>Link copied!</h1>}
-                    <button
-                      type="button"
-                      onClick={ () => this.toggleShare(recipe.type, recipe.id) }
-                    >
-                      <img
-                        src={ shareIcon }
-                        alt="share-btn"
-                        data-testid={ `${index}-horizontal-share-btn` }
-                      />
-                    </button>
+                <Link
+                  to={ `/${recipe.type}s/${recipe.id}` }
+                >
+                  <span
+                    data-testid={ `${index}-horizontal-name` }
+                  >
+                    {recipe.name}
+                  </span>
+                </Link>
 
-                    <button
-                      type="button"
-                      name={ index }
-                      onClick={ (event) => this.makeFave(recipe.type, recipe.id, event) }
-                    >
-                      <img
-                        src={ (favoriteState === true) ? blackHeartIcon : whiteHeartIcon }
-                        alt="Favorite BTN"
-                        data-testid={ `${index}-horizontal-favorite-btn` }
-                      />
-                    </button>
-                  </div>
-                ))
-            )
-          }
-        </div>
+                {copyState && <h1>Link copied!</h1>}
+                <button
+                  type="button"
+                  onClick={ () => toggleShare(recipe.type, recipe.id) }
+                >
+                  <img
+                    src={ shareIcon }
+                    alt="share-btn"
+                    data-testid={ `${index}-horizontal-share-btn` }
+                  />
+                </button>
+
+                <button
+                  type="button"
+                  name={ index }
+                  onClick={ () => makeFave(recipe.id) }
+                >
+                  <img
+                    src={ blackHeartIcon }
+                    alt="Favorite BTN"
+                    data-testid={ `${index}-horizontal-favorite-btn` }
+                  />
+                </button>
+              </div>
+            ))
+        }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default connect(null, null)(FavoriteRecipes);
