df90a1a04c88989c3cd5dea719e054d94bd2c135
cassiojuniorr <cassiospcjunior@gmail.com>
2022-08-30 19:58:47 -0300

    css profile

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import style from '../styles/doneRecipes.module.scss';
+import style from '../styles/profilePeople.module.scss';
 
 class ProfileInfo extends React.Component {
   handleLogout = () => {
@@ -10,30 +10,45 @@ class ProfileInfo extends React.Component {
   render() {
     const userKey = localStorage.getItem('user');
     const userEmail = JSON.parse(userKey);
-    const { containerProfile } = style;
+    const { containerProfile, emailProf, wrapBtn, btnPages } = style;
     return (
       <div className={ containerProfile }>
-        <h5 data-testid="profile-email">{ userEmail?.email }</h5>
-        <Link to="/done-recipes">
-          <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-        </Link>
-        <Link to="/favorite-recipes">
-          <button
-            type="button"
-            data-testid="profile-favorite-btn"
-          >
-            Favorite Recipes
-          </button>
-        </Link>
-        <Link to="/">
-          <button
-            type="button"
-            data-testid="profile-logout-btn"
-            onClick={ () => this.handleLogout() }
-          >
-            Logout
-          </button>
-        </Link>
+        <h5
+          data-testid="profile-email"
+          className={ emailProf }
+        >
+          { userEmail?.email }
+        </h5>
+        <div className={ wrapBtn }>
+          <Link to="/done-recipes">
+            <button
+              type="button"
+              data-testid="profile-done-btn"
+              className={ btnPages }
+            >
+              Done Recipes
+            </button>
+          </Link>
+          <Link to="/favorite-recipes">
+            <button
+              type="button"
+              data-testid="profile-favorite-btn"
+              className={ btnPages }
+            >
+              Favorite Recipes
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              data-testid="profile-logout-btn"
+              className={ btnPages }
+              onClick={ () => this.handleLogout() }
+            >
+              Logout
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
